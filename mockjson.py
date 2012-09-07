e765a8b9f6b4c65334be94d5d149f70e37a9459d
JamesMcMahon <git.spamfriedrice@spamgourmet.com>
2012-09-06 21:06:45 -0400

    replaced number string with built in string

--- mockjson.py
+++ mockjson.py
@@ -64,7 +64,7 @@ def _random_date():
     return datetime.today() - timedelta(days=random.randrange(6571, 27375))
 
 data = {
-    'NUMBER': lambda: _random_item("0123456789"),
+    'NUMBER': lambda: _random_item(string.digits),
     'LETTER_UPPER': lambda: _random_item(string.uppercase),
     'LETTER_LOWER': lambda: _random_item(string.lowercase),
     'MALE_FIRST_NAME': lambda: _random_item(_male_first_name),
